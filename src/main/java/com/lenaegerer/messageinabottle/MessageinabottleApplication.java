dd47d52dd27e41082d7b9680dd483a05b37b07d8
lena <lena@example.com>
2022-09-27 14:31:03 +0200

    spring1

---
+++ src/main/java/com/lenaegerer/messageinabottle/MessageinabottleApplication.java
@@ -0,0 +1,13 @@
+package com.lenaegerer.messageinabottle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageinabottleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageinabottleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lenaegerer/messageinabottle/MessageinabottleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lenaegerer.messageinabottle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageinabottleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
